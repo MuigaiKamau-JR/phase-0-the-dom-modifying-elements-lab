f6ee37c5a0c035d62493791fd5b518f1d9547fd5
Kamau <kamauwamuigaijr@gmail.com>
2024-04-22 12:26:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// My code
+const testVar = "hi"
+
+document.querySelector("main#main").remove();// remove the node element
+
+const newHeader = document.createElement("h1"); // assign the new element to h1
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
